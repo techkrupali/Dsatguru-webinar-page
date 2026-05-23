329159c6c5ded4a11919fd39333fbec13cdca2a7
mjrajput04 <manojvagadiya04@gmail.com>
2026-05-23 11:45:41 +0530

    changes

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = window.location.hostname === 'localhost' 
   ? 'http://localhost:6246' 
-  : 'https://webinar.dsatguru.com/api';
+  : 'https://webinar.dsatguru.com';
 
 export default API_BASE_URL;
